0152962979a45b9f7308b09acb60d04633fc83c2
Yousof <yghahremani@kolombo.com>
2018-08-03 17:46:21 -0400

    Factory Main added

---
+++ Design Patterns/Design Patterns/Factory/Demo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace DotNetDesignPatternDemos.Creational.Factory
+{
+
+    //Start Builder Pattern
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
